fcad1d13b0762192a1e1094e9ddae5c6d57ad42a
Jeffrey Wan <jwan2@ece027.ece.local.cmu.edu>
2019-12-03 23:04:51 -0500

    Added functions for generating only one or two objectives at a time.

--- test_simd/objective_simd.h
+++ test_simd/objective_simd.h
@@ -69,3 +69,53 @@ __m256d objective(w,x,y,z)
   
   return fits;
 }
+
+double objective1(__m256d *x) {
+  int j;
+  // Ellipsoidal with simd
+  __m256d mul_arr[5];
+  __m256d fit_arr;
+  double fit;
+
+  for (j = 0; j < 5; j++)
+    mul_arr[j] = _mm256_mul_pd(x[j], x[j]);
+  for (j = 0; j < 5; j++)
+    mul_arr[j] = _mm256_mul_pd(mul_arr[j], step_arr[j]);
+  mul_arr[0] = _mm256_add_pd(mul_arr[0], mul_arr[1]);
+  mul_arr[2] = _mm256_add_pd(mul_arr[2], mul_arr[3]);
+  mul_arr[1] = _mm256_add_pd(mul_arr[0], mul_arr[2]);
+  mul_arr[3] = _mm256_add_pd(mul_arr[1], mul_arr[4]);
+  fit_arr = _mm256_hadd_pd(mul_arr[3], mul_arr[3]);
+  fit = fit_arr[0] + fit_arr[2];
+
+  return fit;
+}
+
+__m256d objective2(__m256d *x, __m256d *y) {
+  int j;
+  double fit, zero = 0.0;
+  __m256d mul_arr[2];
+  __m256d fit_arr[2];
+  double fit_totals[4];
+  __m256d fits;
+  
+  fit_arr[0] = _mm256_broadcast_sd(&zero);
+  fit_arr[1] = _mm256_broadcast_sd(&zero);
+  for (j = 0; j < 5; j++) {
+    mul_arr[0] = _mm256_mul_pd(x[j], x[j]);
+    mul_arr[1] = _mm256_mul_pd(y[j], y[j]);
+    fit_arr[0] = _mm256_fmadd_pd(mul_arr[0], step_arr[j], fit_arr[0]);
+    fit_arr[1] = _mm256_fmadd_pd(mul_arr[1], step_arr[j], fit_arr[1]);
+  }
+  fit_arr[0] = _mm256_hadd_pd(fit_arr[0], fit_arr[0]);
+  fit_arr[1] = _mm256_hadd_pd(fit_arr[1], fit_arr[1]);
+
+  fit_totals[0] = fit_arr[0][0] + fit_arr[0][2];
+  fit_totals[1] = fit_arr[1][0] + fit_arr[1][2];
+  fit_totals[2] = 0.0;
+  fit_totals[3] = 0.0;
+  // Can also use indices 1 and 3 within each array.
+
+  fits = _mm256_loadu_pd(fit_totals);
+  return fits;
+}
